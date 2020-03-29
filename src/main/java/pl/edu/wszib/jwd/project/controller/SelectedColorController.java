c1dd97e7baab736b9aa9e7dfc946017202898942
Marcin Mikłas <miki@wszib.edu.pl>
2020-03-29 12:54:47 +0200

    Konfiguracja bazy danych

--- src/main/java/pl/edu/wszib/jwd/project/controller/SelectedColorController.java
+++ src/main/java/pl/edu/wszib/jwd/project/controller/SelectedColorController.java
@@ -1,18 +1,31 @@
 package pl.edu.wszib.jwd.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.edu.wszib.jwd.project.dao.SelectedColorDao;
+import pl.edu.wszib.jwd.project.model.SelectedColor;
+import java.util.Date;
 
 @Controller
 public class SelectedColorController {
 
+    @Autowired
+    SelectedColorDao selectedColorDao;
+
     @Value("${app.title.select}")
     private String title;
 
-    @GetMapping("/select")
-    public String selectColorPage(Model model) {
+    @GetMapping({"/select/{color}","/select"})
+    public String selectColorPage(@PathVariable(required = false) String color, Model model) {
+
+        if (!StringUtils.isEmpty(color)) {
+            selectedColorDao.save(new SelectedColor(color, new Date()));
+        }
 
         String[][] colors = {
                 {"red","blue","purple","teal"},
